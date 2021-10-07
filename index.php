e0bf2c62ad65dbf8558e7518d65a80d7365b61a9
PapierPain <gourves.seven@gmail.com>
2021-10-07 14:27:21 +0200

    Init repo

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Super Home</title>
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <meta name="author" content="PapierPain">
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-tofit=no"> -->
+
+        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="/public/main.css">
+    </head>
+
+    <body>
+        <main>
+            <div class="left">
+                <h1>Hack This Page</h1>
+                <p>I'm an pineapple and I can talk with you...</p>
+            </div>
+
+            <div class="right">
+                <ul>
+                    <li>
+                        <a href="https://www.gourves-steven.fr">Portfolio</a>
+                    </li>
+
+                    <li>
+                        <a href="/">This page</a>
+                    </li>
+
+                    <li>
+                        <a href="https://ananas.papierpain.fr">Ananas</a>
+                    </li>
+
+                    <!-- <li>
+                        <a href="https://cloud.papierpain.fr">Cloud</a>
+                    </li> -->
+
+                    <li>
+                        <a href="https://docs.papierpain.fr">Documentations</a>
+                    </li>
+
+                    <li>
+                        <a href="https://bouffe.papierpain.fr">Bouffe</a>
+                    </li>
+
+                    <li>
+                        <a href="https://www.gourves-steven.fr/golden-globe">Golden Globe Race</a>
+                    </li>
+                </ul>
+            </div>
+        </main>
+        
+        <footer>
+        </footer>
+        
+        <!-- <script src="/public/main.js"></script> -->
+    </body>
+</html>
