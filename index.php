e46f109ffaeab05a6acb1012bee6ff75dfd33bb8
Steven Gourves <gourves.seven@gmail.com>
2022-02-08 22:30:03 +0000

    Update routes.json, index.php, README.md, .gitlab-ci.yml, public/main.css Deleted .htaccess, .drone.yml

--- index.php
+++ index.php
@@ -3,14 +3,12 @@
 <html lang="fr">
     <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Super Home</title>
+        <title>Labo - Papierpain</title>
 
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
         <meta name="author" content="PapierPain">
-        <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-tofit=no"> -->
 
-        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href="/public/main.css">
@@ -18,7 +16,7 @@
 
     <body>
         <main>
-            <h1>Du PAPIER avec du PAIN</h1>
+            <h1>Labo papierpain</h1>
 
             <div>
                 <ul>
